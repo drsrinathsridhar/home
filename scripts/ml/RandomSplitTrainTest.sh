ecb2902d92e1fa71e9e1e351c0f60dbeffcbf641
Srinath Sridhar <ssridhar@wks-14-37.mpi-inf.mpg.de>
2014-02-23 15:42:41 +0100

    Added awesome Schwartzian transform based randomization.

---
+++ scripts/ml/RandomSplitTrainTest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script is for splitting data into train and test sets
+# Works only if data is in one file and labels (structured or otherwise)
+# are in another file.
+# The percentage of train/test samples can be specified
+# [ USAGE ]: $0 <Data_File> <Labels_File> <TrainPercent>
+if [ $# -ne 3 ]; then
+    echo "[ USAGE ]: $0 <Data_File> <Labels_File> <TrainPercent>"
+    exit
+fi
+
+DataFile=$1
+LabelFile=$2
+nLines=$(wc -l ${DataFile} | cut -f1 -d' ')
+TrainPercent=$3
+nTrainLines=$(( ${nLines} * ${TrainPercent} / 100 ))
+echo "Creating ${nTrainLines} training samples."
+nTestLines=$(( ${nLines} - ${nTrainLines} ))
+echo "Creating ${nTestLines} testing samples."
+
+# First generate a sequence of numbers and shuffle it to get random indices within range
+seq 1 ${nTrainLines} | shuf > random_train.txt
+seq $(( ${nTrainLines} + 1 )) ${nLines} | shuf > random_test.txt 
+
+paste <(cat random_train.txt) <(head -${nTrainLines} ${DataFile}) | sort | cut -f2- > ${DataFile}_TrainData.txt
+paste <(cat random_train.txt) <(head -${nTrainLines} ${LabelFile}) | sort | cut -f2- > ${LabelFile}_TrainLabels.txt
+
+paste <(cat random_test.txt) <(sed -n "$(( ${nTrainLines} + 1 )),${nLines}p" ${DataFile}) | sort | cut -f2- > ${DataFile}_TestData.txt
+paste <(cat random_test.txt) <(sed -n "$(( ${nTrainLines} + 1 )),${nLines}p" ${LabelFile}) | sort | cut -f2- > ${LabelFile}_TestLabels.txt
+
+# Cleanup
+rm random_*
