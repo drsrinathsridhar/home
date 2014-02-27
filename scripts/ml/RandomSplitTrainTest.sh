0f64a08b073377ae7e8cb7b6a9996ea8847b72a6
Srinath Sridhar <ssridhar@wks-14-37.mpi-inf.mpg.de>
2014-02-27 18:02:12 +0100

    Changes to .emacs.d

--- scripts/ml/RandomSplitTrainTest.sh
+++ scripts/ml/RandomSplitTrainTest.sh
@@ -4,11 +4,16 @@
 # are in another file.
 # The percentage of train/test samples can be specified
 # [ USAGE ]: $0 <Data_File> <Labels_File> <TrainPercent>
-if [ $# -ne 3 ]; then
-    echo "[ USAGE ]: $0 <Data_File> <Labels_File> <TrainPercent>"
+if [ $# -lt 3 ] || [ $# -gt 4 ]; then
+    echo "[ USAGE ]: $0 <Data_File> <Labels_File> <TrainPercent> [<RandomizeTestLabels = 0 (default) | 1>]"
     exit
 fi
 
+if [ $# -eq 4 ] && [ $4 -eq 1 ]; then
+    isRandomizeTest=1
+else
+    isRandomizeTest=0
+fi
 DataFile=$1
 LabelFile=$2
 nLines=$(wc -l ${DataFile} | cut -f1 -d' ')
@@ -19,13 +24,20 @@ nTestLines=$(( ${nLines} - ${nTrainLines} ))
 echo "Creating ${nTestLines} testing samples."
 
 # First generate a sequence of numbers and shuffle it to get random indices within range
-seq 1 ${nLines} | shuf > random.txt
+if [ ${isRandomizeTest} -eq 1 ]; then
+    echo "Randomizing test labels."
+    seq 1 ${nLines} | shuf > random.txt
+else
+    echo "NOT randomizing test labels."
+    seq 1 ${nTrainLines} | shuf > random.txt
+    seq $(( ${nTrainLines} + 1 )) ${nLines} >> random.txt
+fi
 
-paste <(cat random.txt) <(cat ${DataFile}) | sort | head -${nTrainLines} | cut -f2- > $(dirname ${DataFile})/TrainData.txt
-paste <(cat random.txt) <(cat ${LabelFile}) | sort | head -${nTrainLines} | cut -f2- > $(dirname ${LabelFile})/TrainLabels.txt
+paste <(cat random.txt) <(cat ${DataFile}) | sort -n | head -${nTrainLines} | cut -f2- > $(dirname ${DataFile})/TrainData.txt
+paste <(cat random.txt) <(cat ${LabelFile}) | sort -n | head -${nTrainLines} | cut -f2- > $(dirname ${LabelFile})/TrainLabels.txt
 
-paste <(cat random.txt) <(cat ${DataFile}) | sort | sed -n "$(( ${nTrainLines} + 1 )),${nLines}p" | cut -f2- > $(dirname ${DataFile})/TestData.txt
-paste <(cat random.txt) <(cat ${LabelFile}) | sort | sed -n "$(( ${nTrainLines} + 1 )),${nLines}p" | cut -f2- > $(dirname ${LabelFile})/TestLabels.txt
+paste <(cat random.txt) <(cat ${DataFile}) | sort -n | sed -n "$(( ${nTrainLines} + 1 )),${nLines}p" | cut -f2- > $(dirname ${DataFile})/TestData.txt
+paste <(cat random.txt) <(cat ${LabelFile}) | sort -n | sed -n "$(( ${nTrainLines} + 1 )),${nLines}p" | cut -f2- > $(dirname ${LabelFile})/TestLabels.txt
 
 # Cleanup
 rm random.txt
