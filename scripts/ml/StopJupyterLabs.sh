a70f4e3b5faba04160cb29320c592233826a14d4
Srinath Sridhar <drsrinathsridhar@gmail.com>
2018-04-15 16:35:14 -0700

    Updated stopping commands

--- scripts/ml/StopJupyterLabs.sh
+++ scripts/ml/StopJupyterLabs.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-pkill -f "jupyter lab --no-browser --port=4242"
+if [ $# -gt 1 ]; then
+	echo "[ USAGE ]: $0 [PortNumber=4242]"
+	exit
+fi
+
+PortNumber=4242
+if [ $# -eq 1 ]; then
+	PortNumber=${1}
+fi
+
+pkill -f "jupyter-lab --no-browser --port=${PortNumber}"
