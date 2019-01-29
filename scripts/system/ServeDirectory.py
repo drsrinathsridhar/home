5fec15959e6748b143d772ba898887b4eeb00cc2
Srinath Sridhar <ssrinath@orion-dgx.stanford.edu>
2019-01-28 16:52:57 -0800

    Added python file serving

---
+++ scripts/system/ServeDirectory.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map.update({
+    '.jpg': 'application/x-web-app-manifest+json',
+});
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("Serving at port", PORT)
+httpd.serve_forever()
